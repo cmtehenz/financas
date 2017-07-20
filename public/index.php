2331901e49d882a054b0e036f2d4e9e4b2d48734
Gustavo Costa <cmtehenz@gmail.com>
2017-07-20 18:26:33 -0300

    Criando categorias de custo

--- public/index.php
+++ public/index.php
@@ -24,20 +24,24 @@ $app->get('/home/{name}/{id}', function(ServerRequestInterface $request){
     return $response;
 });
 
-$meuModel = new \SONFin\Models\CategoryCost();
 
-
-$app->get('/category-costs', function() use($app){
-    $meuModel = new CategoryCost();
-    $categories = $meuModel->all();
-    $view = $app->service('view.renderer');
-    return $view->render('category-costs/list.html.twig',[
-        'categories' => $categories
-    ]);
-});
-
-$app->get('/home', function() {
-    echo "Mostrando a home!!";
-});
+$app
+    ->get('/category-costs', function() use($app){
+        $meuModel = new CategoryCost();
+        $categories = $meuModel->all();
+        $view = $app->service('view.renderer');
+        return $view->render('category-costs/list.html.twig',[
+            'categories' => $categories
+        ]);
+    })
+    ->get('/category-costs/new', function() use($app){
+        $view = $app->service('view.renderer');
+        return $view->render('category-costs/create.html.twig');
+    })
+    ->post('/category-costs/store', function(ServerRequestInterface $request){
+        $data = $request->getParsedBody();
+        SONFin\Models\CategoryCost::create($data);
+        return new \Zend\Diactoros\Response\RedirectResponse('/category-costs');
+    });
 
 $app->start();
--- src/Models/CategoryCost.php
+++ src/Models/CategoryCost.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class CategoryCost extends Model
 {
-
+    // Mass Assignment
+    protected $fillable = ["id","name"];
 }
