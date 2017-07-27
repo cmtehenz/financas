0eb4b61d537a29a4950237d0897a7bdb281ad4ad
Gustavo Costa <cmtehenz@gmail.com>
2017-07-27 15:23:34 -0300

    Iniciando crud de contas a pagar

---
+++ db/seeds/BillPaysSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class BillPaysSeeder extends AbstractSeed
+{
+    /**
+     * @var \Illuminate\Database\Eloquent\Collection
+     */
+    private $categories;
+
+
+    public function run()
+    {
+        require __DIR__ . '/../bootstrap.php';
+        $this->categories = \SONFin\Models\CategoryCost::all();
+
+        $faker = \Faker\Factory::create('pt_BR');
+        $faker->addProvider($this);
+        $billPays = $this->table('bill_pays');
+        $data = [];
+        foreach (range(1, 20) as $value) {
+            $userId = rand(1,4);
+            $data[] = [
+                'date_launch' => $faker->date(),
+                'name' => $faker->word(),
+                'user_id' => rand(1,4),
+                'value' => $faker->randomFloat(2,10,5000),
+                'category_cost_id' => $faker->categoryId($userId),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ];
+        }
+        $billPays->insert($data)->save();
+    }
+
+    public function categoryId($userId){
+        $categories = $this->categories->where('user_id', $userId);
+        $categories = $categories->pluck('id');
+        return \Faker\Provider\Base::randomElement($categories->toArray());
+    }
+
+
+}
---
+++ src/Models/BillPay.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SONFin\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BillPay extends Model
+{
+    protected $fillable = [
+        'date_launch',
+        'name',
+        'value',
+        'user_id',
+        'category_cost_id'
+    ];
+}
