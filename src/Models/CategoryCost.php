f43e189962559a426cb50ea80e3649e70ac0f470
Gustavo Costa <cmtehenz@gmail.com>
2017-07-27 11:26:00 -0300

    Adicionando usuarios às categorias

--- src/Models/CategoryCost.php
+++ src/Models/CategoryCost.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class CategoryCost extends Model
 {
     // Mass Assignment
-    protected $fillable = ["id","name"];
+    protected $fillable = ["id","name", "user_id"];
 }
