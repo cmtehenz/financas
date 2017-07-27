2b65b6b3dd92451a1be5316db208337ba20e5e47
Gustavo Costa <cmtehenz@gmail.com>
2017-07-27 11:52:13 -0300

    CRUD de categorias com usuários

--- src/Repository/DefaultRepository.php
+++ src/Repository/DefaultRepository.php
@@ -15,8 +15,6 @@ class DefaultRepository implements RepositoryInterface
      * @var Model
      */
     private $_model;
-
-
     /**
      * DefaultRepository constructor.
      *
@@ -27,41 +25,47 @@ class DefaultRepository implements RepositoryInterface
         $this->_modelClass = $modelClass;
         $this->_model = new $modelClass;
     }
-
     public function all(): array
     {
         return $this->_model->all()->toArray();
     }
-
     public function create(array $data)
     {
         $this->_model->fill($data);
         $this->_model->save();
         return $this->_model;
     }
-
-    public function update(int $id, array $data)
+    public function update($id, array $data)
     {
-        $model = $this->find($id);
+        $model = $this->findInternal($id);
         $model->fill($data);
         $model->save();
         return $model;
     }
-
-    public function delete(int $id)
+    public function delete($id)
     {
-        $model = $this->find($id);
+        $model = $this->findInternal($id);
         $model->delete();
     }
-
+    protected function findInternal($id)
+    {
+        return is_array($id) ? $model = $this->findOneBy($id) : $this->find((int)$id);
+    }
     public function find(int $id, bool $failIfNotExist = true)
     {
         return $failIfNotExist ? $this->_model->findOrFail($id) :
             $this->_model->find($id);
     }
-
     public function findByField(string $field, $value)
     {
         return $this->_model->where($field, '=', $value)->get();
+    }
+    public function findOneBy(array $search)
+    {
+        $queryBuilder = $this->_model;
+        foreach ($search as $field => $value) {
+            $queryBuilder = $queryBuilder->where($field, '=', $value);
+        }
+        return $queryBuilder->firstOrFail();
     }
 }
